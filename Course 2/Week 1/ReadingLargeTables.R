4f9c8dbce3ca78a64557fc13856a3d983b2917d9
migduroli <migduroli@gmail.com>
2016-06-30 19:11:48 +0100

    Update Course 2

---
+++ Course 2/Week 1/ReadingLargeTables.R
@@ -0,0 +1,3 @@
+# Reading large tables
+
+## HINT 1: if your data set is bigger than your RAM --> STOP!
--- Course 2/Week 1/ReadingTabularData.R
+++ Course 2/Week 1/ReadingTabularData.R
@@ -16,3 +16,18 @@
 ## save [for reading saved workspaces]
 ## serialize [for reading single R objects in binary form]
 
+# Reading with read.table (arguments)
+
+## file = " ... "
+## header = TRUE or FALSE
+## sep = "..." [can be space whatever set of characters/string]
+## nrows = number of rows in the data set
+## comment.char = set of characters identifying the comments on a data set
+## skip = number of lines to skip before beginning
+## stringAsFactors = should character variables be coded as factors? (default TRUE!!!)
+
+# Example
+data <- read.table("foo.txt") # default separator [space]
+data <- read.csv("foo.txt") # the only difference with read.table is the default separator [comma]
+
+
